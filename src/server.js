c82ebea5f38ba4f5ef860482c70abb7606921943
lbourke1234 <lbourke1234@gmail.com>
2022-05-17 14:55:27 +0100

    push be

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import listEndpoints from 'express-list-endpoints'
 import productsRouter from './apis/products/index.js'
 import cors from 'cors'
 import { join } from 'path'
+import createError from 'http-errors'
 
 const publicFolderPath = join(process.cwd(), './public')
 
@@ -10,14 +11,26 @@ const server = express()
 
 const port = process.env.PORT || 5001
 
-const whitelist = []
+const whitelist = [process.env.BE_URL]
 const corsOptions = {
-  origin: 'http://frontendapp.com'
+  origin: (origin, next) => {
+    console.log('CURRENT ORIGIN: ', origin)
+
+    if (!origin || whitelist.indexOf() !== -1) {
+      next(null, true)
+    } else {
+      next(
+        createError(
+          400,
+          `Cors Error! your origin ${origin} is not in the list!`
+        )
+      )
+    }
+  }
 }
 
-server.use(cors())
-
 server.use(express.static(publicFolderPath))
+server.use(cors(corsOptions))
 server.use(express.json())
 
 server.use('/products', productsRouter)
